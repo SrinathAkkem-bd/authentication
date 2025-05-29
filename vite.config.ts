ca4c310ded087394e9fcc59f0849b59fcd827865
SrinathAkkem-bd <infipara.edu@gmail.com>
2025-05-29 20:26:21 +0530

    Fix Vite configuration for network access

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
   },
   server: {
-    host: true, // Change this to enable network access
+    host: '0.0.0.0',
     port: 3000,
     strictPort: true,
     headers: {
